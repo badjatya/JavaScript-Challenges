e7a7397d2acd4a761cdcebfdc28d4cc83131fc5b
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-01-21 16:22:43 +0530

    hit button random card and deal button created

--- static/js/script.js
+++ static/js/script.js
@@ -180,3 +180,55 @@ function randomButton () {
 }
 
 
+// CHALLENGE 5 
+let blackjackGame = {
+    'you': {
+        'scoreSpan': '#your-blackjack-result',
+        'div': '#your-box',
+        'score': 0,
+    },
+    'dealer': {
+        'scoreSpan': '#dealer-blackjack-result',
+        'div': '#dealer-box',
+        'score': 0,
+    },
+    'cards': ['2','3','4','5','6','7','8','9','J','Q','K','A'],
+}
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+const hitSound = new Audio('./static/sounds/swish.m4a');
+
+document.querySelector("#blackjack-hit-button").addEventListener('click',hitButtonBlackjack);
+document.querySelector("#blackjack-deal-button").addEventListener('click',dealButtonBlackjack);
+
+function hitButtonBlackjack () {
+    let card = randomCard();
+    showCard(DEALER , card);
+}
+
+function showCard (activePlayer , card) {
+    let cardImage = document.createElement("img");
+    cardImage.src = `./static/img/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+
+    hitSound.play();
+}
+
+function dealButtonBlackjack () {
+    let yourImage = document.querySelector("#your-box").querySelectorAll("img");
+    let dealerImage = document.querySelector("#dealer-box").querySelectorAll("img");
+
+    for (let i = 0; i < yourImage.length; i++){
+        yourImage[i].remove();
+    }
+
+    for (let i = 0; i < dealerImage.length; i++){
+        dealerImage[i].remove();
+    }
+}
+
+function randomCard () {
+    let randomIndex = Math.floor(Math.random() * 13);
+    return blackjackGame['cards'][randomIndex];
+}
